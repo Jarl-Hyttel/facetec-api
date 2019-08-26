2a2dc83531ed77cf3f4ad7da179b42ac92925747
Jarl Hyttel <jarl.hyttel@gmail.com>
2019-08-26 14:04:43 +0200

    Initial commit

---
+++ controllers/Image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '65fe44751fd24f1c9fcb27bf5080b0ce'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+//  UPDATE COMMENT Reason for not using "from" explained previous
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/Register');
+const signin = require('./controllers/Signin');
+const profile = require('./controllers/Profile');
+const image = require('./controllers/Image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : 'postgres',
+		password : 'test',
+		database : 'face-tec'
+  	}
+});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) })
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put("/image", (req, res) => { image.handleImage(req, res, db) })
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
